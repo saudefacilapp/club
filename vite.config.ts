97097b86925f588cd44ec66e4d7362f9f0c7aaf6
saudefacilapp <dededanilo002@gmail.com>
2025-09-10 02:09:52 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
+  // Adicione a linha abaixo para corrigir o caminho no GitHub Pages
+  base: '/club/',
+
   server: {
-    host: "::",
-    port: 8080,
+    host: ':',
+    port: 3000,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()], // Deixei a parte do "lovable-tagger" mais limpa
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-}));
+});
